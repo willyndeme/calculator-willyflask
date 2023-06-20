add8dbeed6e0f105680e48589a6ebcc53e35cc23
willyndeme <136729994+willyndeme@users.noreply.github.com>
2023-06-20 13:58:44 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,5 @@
-#from calculator import create_app
-from calculator import app
-#app=create_app()
+from calculator import create_app
+app=create_app()
 
 if __name__=='__main__':
     app.run(debug=False)
